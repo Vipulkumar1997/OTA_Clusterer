195759a529b5ee6d7dadebae5fb73e454b17cc30
smarterty <sandro.cilurzo@gmail.com>
2017-11-07 17:37:21 +0100

    implementation of scrolling of documents (returns all)
    - scrolling implemented
    - code refactoring

--- Web Crawling/PythonElasticSearchClient/ElasticSearchClient.py
+++ Web Crawling/PythonElasticSearchClient/ElasticSearchClient.py
@@ -1,49 +1,87 @@
 from elasticsearch import Elasticsearch
 import json
+import time
+import random
+import os
+import errno
 
-#Initialization
+# constants definition
 ELASTIC_SERVER = 'http://mse-2017-wbcilurz.el.eee.intern:9200'
-es = Elasticsearch([ELASTIC_SERVER])
+RESPONSE_FILE_PATH = 'Responses'
+
+
+def initialize_elasticsearch():
+    es = Elasticsearch([ELASTIC_SERVER])
+    return es
+
+
+def create_response_file_directory(hostname):
+    if not os.path.exists(hostname):
+        try:
+            os.makedirs(RESPONSE_FILE_PATH + '/' + hostname)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
 
 
 def get_document_content():
-    res = es.search(index="fess.search", body={"query": {"match_all": {}}})
-    print("Got %d Hits:" % res['hits']['total'])
+    es = initialize_elasticsearch()
+    result = es.search(index="fess.search",
+                    body={
+                        "query": {
+                            "match_all": {
 
-    for results in res['hits']['hits']:
-        print(results['_source']['content'])
-
-
-def get_documents_of_host(hostname):
-    result = es.search(index="fess.search", body={
-        "query": {
-
-            "constant_score": {
-
-                "filter": {
-
-                    "term": {
-
-                        "host": hostname
-
-                    }
-
-                }
-
-            }
-
-        }
-
-    })
+                            }
+                        }
+                    })
 
     print("Got %d Hits:" % result['hits']['total'])
 
-    i = 0
     for entry in result['hits']['hits']:
-        filename = hostname + '_' + str(i) + '.json'
-        with open('Responses/' + filename, 'w') as output:
+        print(entry['_source']['content'])
+
+
+def get_documents_of_host(hostname):
+    es = initialize_elasticsearch()
+    print("start fetching data from elasticsearch...")
+    result = es.search(index="fess.search",
+                       scroll='10m',
+                       size=1000,
+                       body={
+                           "query": {
+
+                               "constant_score": {
+
+                                   "filter": {
+
+                                       "term": {
+
+                                           "host": hostname
+
+                                       }
+
+                                   }
+
+                               }
+
+                           }
+
+                       })
+
+    print("Got %d Hits:" % result['hits']['total'])
+    print("start json dumping...")
+
+    create_response_file_directory(hostname)
+
+    for entry in result['hits']['hits']:
+        filename = hostname + '_' + time.strftime("%Y%m%d") + str(random.randint(0, 1000)) + '_' + '.json'
+        with open(RESPONSE_FILE_PATH + '/' + hostname + '/' + filename, 'w') as output:
             json.dump(entry['_source'], output)
-        i += 1
 
 
-get_documents_of_host("www.agoda.com")
+def main():
+    get_documents_of_host(hostname="www.agoda.com")
+
+
+if __name__ == "__main__":
+    main()
