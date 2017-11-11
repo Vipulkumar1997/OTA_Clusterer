c178338df79d343475af1e87442e6b6fa4513cd2
Sandro <sandro.cilurzo@gmail.com>
2017-11-11 20:23:21 +0100

    refactored everything and made it works!

--- Web Crawling/FessAPI-Client/FessAPI.py
+++ Web Crawling/FessAPI-Client/FessAPI.py
@@ -71,28 +71,88 @@ def prepare_urls():
     prepared_urls = []
     for url in urls_list:
         url = url[0]
-        if re.match("^http?://", string=url):
-            prepared_urls.append(url)
-
-            https_url = url.replace("http://", "https://")
-            prepared_urls.append(https_url)
-
-            empty_url = url.replace("http://", "")
-            if re.match("www.", empty_url):
-                www_url = empty_url
-                prepared_urls.append(www_url)
-            else:
-                www_url = "".join(("www", empty_url))
-                prepared_urls.append(www_url)
+        formatted_urls = check_url_format(url)
+        prepared_urls.append(formatted_urls)
 
     with open("URLS/url_prepared.csv", "w") as file:
         writer = csv.writer(file, delimiter="\n")
-        writer.writerow(prepared_urls)
+        for row in prepared_urls:
+            writer.writerow(row)
+
+
+def check_url_format(url):
+    urls = []
+    if re.match("^http?://", string=url):
+        formatted_http_urls = [get_formatted_url_list(url, "http://")]
+        urls = formatted_http_urls
+
+    elif re.match("^https?://", string=url):
+        formatted_https_urls = [get_formatted_url_list(url, "https://")]
+        urls = formatted_https_urls
+
+    return urls
+
+
+def get_formatted_url_list(url, url_beginning):
+    formatted_url_list = []
+    if not check_if_url_ends_with_slash(url):
+        url = get_url_with_slash(url)
+        formatted_url_list.append(url)
+
+    else:
+        formatted_url_list.append(url)
+
+    if url_beginning == "http://":
+        https_url = replace_url_beginning(url, "http", url_beginning)
+        formatted_url_list.append(https_url)
+
+        www_url = replace_url_beginning(url, "www", url_beginning)
+        formatted_url_list.append(www_url)
+
+    elif url_beginning == "https://":
+        http_url = replace_url_beginning(url, "https", url_beginning)
+        formatted_url_list.append(http_url)
+
+        www_url = replace_url_beginning(url, "www", url_beginning)
+        formatted_url_list.append(www_url)
+
+    wild_card_url = get_wildcard_url(url)
+    formatted_url_list.append(wild_card_url)
+
+    return formatted_url_list
+
+
+def check_if_url_ends_with_slash(url):
+    if re.search(r"/$", url):
+        return True
+
+
+def replace_url_beginning(url, option, url_beginning):
+    if option == "http":
+        url = url.replace(url_beginning, "https://")
+
+    elif option == "https":
+        url = url.replace(url_beginning, "http://")
+
+    elif option == "www":
+        url = url.strip(url_beginning)
+
+    return url
+
+
+def get_url_with_slash(url):
+    url_with_slash = "".join((url, "/"))
+    return url_with_slash
+
+
+def get_wildcard_url(url):
+    url_wildcard = "".join((url, ".*"))
+    return url_wildcard
 
 
 def main():
     prepare_urls()
-    create_new_crawler()
+    # create_new_crawler()
 
 
 if __name__ == "__main__":
