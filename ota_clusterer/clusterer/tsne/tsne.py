a0397fca6e711bfb35cefb3617a73fbc3a3f4a6f
Sandro <sandro.cilurzo@gmail.com>
2017-11-27 08:54:49 +0100

    affinity propagation clustering implemented

--- ota_clusterer/clusterer/tsne/tsne.py
+++ ota_clusterer/clusterer/tsne/tsne.py
@@ -1,5 +1,6 @@
 import logging
 import sklearn.manifold
+import sklearn.cluster
 import pandas as pd
 import seaborn as seaborn
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ from ota_clusterer.doc2vec import doc2vec
 import numpy as np
 import time
 from ota_clusterer import settings
+from itertools import cycle
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -64,6 +66,45 @@ def create_doc_model_plot(tsne_doc_model, labels, filename):
     plt.savefig(file_path + file_name)
 
 
+def create_affinity_propagation_cluster_doc2vec_plot(doc2vec_tsne_model, fnames, filename):
+    affinity_propagation = sklearn.cluster.AffinityPropagation().fit(doc2vec_tsne_model)
+
+    cluster_centers_indices = affinity_propagation.cluster_centers_indices_
+    labels = affinity_propagation.labels_
+    n_clusters_ = len(cluster_centers_indices)
+
+    plt.figure(num=1, figsize=(80, 80), facecolor="w", edgecolor="k")
+    colors = cycle("bgrcmyk")
+
+    for k, col in zip(range(n_clusters_), colors):
+        class_members = labels == k  # class_members ist array von boolschen werten, beschreibt cluster membership
+        cluster_center = doc2vec_tsne_model[cluster_centers_indices[k]]
+
+        fnames_cluster = []
+        fname_indices = [i for i, x in enumerate(class_members) if x]
+        for i in fname_indices: fnames_cluster.append(fnames[i])
+
+        # print(fnames_cluster)
+        # print(len(class_members))
+        # print(len(fnames))
+        # print(cluster_center)
+
+        plt.plot(doc2vec_tsne_model[class_members, 0], doc2vec_tsne_model[class_members, 1], col + ".")
+        plt.plot(cluster_center[0], cluster_center[1], "o", markerfacecolor=col, markersize=20)
+
+        # plt.annotate(fnames[labels[k]], (cluster_center[0], cluster_center[1]), xytext=(0, -8),
+        #        textcoords="offset points", va="center", ha="left")
+
+        for x, fname in zip(doc2vec_tsne_model[class_members], fnames_cluster):
+            plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col, linestyle='--', linewidth=1)
+            plt.annotate(fname, (x[0], x[1]), xytext=(0, -8),
+                         textcoords="offset points", va="center", ha="left")
+
+    file_path = settings.DATA_DIR + "experiments/t-sne/"
+    file_name = 'affinity_propagation_cluster-' + filename + "-" + time.strftime("%d-%b-%Y-%X") + ".png"
+    plt.savefig(file_path + file_name, facecolor="w", dpi=90)
+
+
 def load_tsne_vector_model(modelname):
     tsne_vector_model = np.load(modelname)
     return tsne_vector_model
@@ -96,9 +137,14 @@ def generate_doc2vec_plot(doc2vec_model, tsne_model_name=None):
 def main():
     doc2vec_model = doc2vec.load_existing_model('doc2vecmodel-25-Nov-2017-11:58:06')
 
-    generate_word2vec_plot(doc2vec_model, 'wordvectors-25-Nov-2017-16:05:48-array.npy')
-    generate_doc2vec_plot(doc2vec_model, 'docvectors-iter-10000-25-Nov-2017-12:03:40-array.npy')
+    # generate_word2vec_plot(doc2vec_model, 'wordvectors-25-Nov-2017-16:05:48-array.npy')
+    # generate_doc2vec_plot(doc2vec_model, 'docvectors-iter-10000-25-Nov-2017-12:03:40-array.npy')
 
+    labels = list(doc2vec_model.docvecs.doctags.keys())
+    # doc2vec_vector_model = create_2d_docvec_tsne_model(doc2vec_model, 'cluster-doc2vec-')
+    doc2vec_vector_matrix = doc2vec.create_doc_vector_matrix(doc2vec_model)
+    doc2vec_vector_model = create_2d_tsne_model(doc2vec_vector_matrix, 'cluster-doc2vec-')
+    create_affinity_propagation_cluster_doc2vec_plot(doc2vec_vector_model, labels, 'cluster-doc2vec-')
 
 
 if __name__ == "__main__":
