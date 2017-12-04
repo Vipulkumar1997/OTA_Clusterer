e95a4420247c52646e7030c571118f804e485eac
Sandro <sandro.cilurzo@gmail.com>
2017-12-04 16:05:36 +0100

    working on setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='OTAClusterer',
       version='0.1',
       description='OTA Clusterer',
       author='Sandro Cilurzo',
       author_email='sandro.cilurzo@gmail.cpm',
-      packages=['ota_clusterer'],
+      packages=find_packages(),
      )
